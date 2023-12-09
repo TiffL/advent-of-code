ae42b2440fbe011fc36be990826e1997faf2e18c
TiffL <TiffL@users.noreply.github.com>
2023-12-09 14:06:20 -0500

    complete day 9

---
+++ 2023/day-9/part-1.py
@@ -0,0 +1,31 @@
+# --- Day 9: Mirage Maintenance ---
+import re
+
+
+def get_next_value(sequence):
+    latest_sequence = [int(item) for item in sequence]
+    total = 0
+
+    while any([item != 0 for item in latest_sequence]):
+        total += latest_sequence[-1]
+
+        new_sequence = []
+        for i, item in enumerate(latest_sequence):
+            if i < len(latest_sequence)-1:
+                new_sequence.append(latest_sequence[i+1]-item)
+
+        latest_sequence = new_sequence
+
+    return total
+
+
+def main():
+    INPUT_FILE = "input.txt"
+
+    with open(INPUT_FILE) as file:
+        result = sum([get_next_value(line.strip().split(" ")) for line in file])
+        print(result)
+
+
+if __name__ == "__main__":
+    main()
